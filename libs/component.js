33d389ccbecf6d2142569b24f578f36e13ff1dd5
Dario Sechi <dario.sechi.7900@gmail.com>
2019-02-22 19:01:04 +0100

    Create the Component module

---
+++ libs/component.js
@@ -0,0 +1,69 @@
+const fs = require('fs-extra')
+const _ = require('lodash')
+const regex = require('./regex')
+const mediaQueries = require('../assets/media-queries.json')
+
+class Component {
+  constructor (_name, _classes, _colors) {
+    this.name = _name
+    this.colors = _colors
+    this.classes = _classes
+  }
+
+  parse () {
+    let classes = {}
+
+    if (_.has(this.classes, 'int')) {
+      _.forEach(this.classes.int, (_property, _name) => {
+        for (let i = 0; i <= 100; i++) {
+          let name, property
+
+          name = _name + i
+          property = _property.replace(regex.int, i)
+
+          classes[name] = property
+        }
+      })
+    }
+
+    if (_.has(this.classes, 'color')) {
+      _.forEach(this.classes.color, (_property, _name) => {
+        this.colors.forEach((color) => {
+          let name, property
+
+          name = _name + color
+          property = _property.replace(regex.color, color)
+
+          classes[name] = property
+        })
+      })
+    }
+
+    this.classes = classes
+  }
+
+  toCss () {
+    let css = ''
+
+    _.forEach(mediaQueries, (prefix, media) => {
+      _.forEach(this.classes, (property, name) => {
+        css += `${prefix}.${media}${name} ${property} ${prefix && media ? '}' : ''}`
+      })
+    })
+
+    this.css = css.replace(/\s/gm, '')
+  }
+
+  write () {
+    if (!fs.existsSync('./dist')) fs.mkdirSync('./dist')
+    fs.writeFileSync(`./dist/${this.name}.css`, this.css)
+  }
+
+  build () {
+    this.parse()
+    this.toCss()
+    this.write()
+  }
+}
+
+module.exports = Component
