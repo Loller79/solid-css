df03af2431f2c7948de7e5f1816026e6c7300032
Dario Sechi <dario.sechi.7900@gmail.com>
2019-02-22 19:21:30 +0100

    Fix missing } at the end of the Container classes

--- components/container.js
+++ components/container.js
@@ -5,15 +5,15 @@ const data = {
     'cna': '{ width: auto; height: auto }'
   },
   int: {
-    'cn': '{ width: $INTpx; height: $INTpx',
-    'mncn': '{ min-width: $INTpx; min-height: $INTpx',
-    'mxcn': '{ max-width: $INTpx; max-height: $INTpx',
-    'cnp': '{ width: $INT%; height: $INT%',
-    'mncnp': '{ min-width: $INT%; min-height: $INT%',
-    'mxcnp': '{ max-width: $INT%; max-height: $INT%',
-    'cnd': '{ width: calc(var(--vh) * $INT); height: calc(var(--vh) * $INT)',
-    'mncnd': '{ min-width: calc(var(--vh) * $INT); min-height: calc(var(--vh) * $INT)',
-    'mxcnd': '{ max-width: calc(var(--vh) * $INT); max-height: calc(var(--vh) * $INT)'
+    'cn': '{ width: $INTpx; height: $INTpx }',
+    'mncn': '{ min-width: $INTpx; min-height: $INTpx }',
+    'mxcn': '{ max-width: $INTpx; max-height: $INTpx }',
+    'cnp': '{ width: $INT%; height: $INT% }',
+    'mncnp': '{ min-width: $INT%; min-height: $INT% }',
+    'mxcnp': '{ max-width: $INT%; max-height: $INT% }',
+    'cnd': '{ width: calc(var(--vh) * $INT); height: calc(var(--vh) * $INT) }',
+    'mncnd': '{ min-width: calc(var(--vh) * $INT); min-height: calc(var(--vh) * $INT) }',
+    'mxcnd': '{ max-width: calc(var(--vh) * $INT); max-height: calc(var(--vh) * $INT) }'
   }
 }
 
