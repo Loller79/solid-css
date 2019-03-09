b517e6c0634c9a04986c9377b10f5c3f7a8bbe40
Dario Sechi <dario.sechi.7900@gmail.com>
2019-03-09 12:51:17 +0100

    Add more display variants

--- components/display.js
+++ components/display.js
@@ -4,6 +4,9 @@ const data = {
   normal: {
     'df': '{ display: flex }',
     'dn': '{ display: none }',
+    'db': '{ display: block }',
+    'di': '{ display: inline }',
+    'dib': '{ display: inline-block }',
     'pen': '{ pointer-events: none }',
     'pea': '{ pointer-events: all }'
   }
