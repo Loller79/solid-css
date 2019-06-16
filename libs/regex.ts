73e372dfacaf055f6fc42c34f7a4a85a7def9c51
Dario Sechi <dario.sechi.7900@gmail.com>
2019-06-16 17:53:33 +0200

    Add support to tsx and vue extensions

--- libs/regex.ts
+++ libs/regex.ts
@@ -15,7 +15,7 @@ const regex: Regex = {
   int: new RegExp(/\$INT/, 'gm'),
   color: new RegExp(/\$COLOR/, 'gm'),
   media: new RegExp(/(sm-|md-|lg-|xl-)/, 'gm'),
-  type: new RegExp(/\.(html|js|jsx|ts)$/, 'gm'),
+  type: new RegExp(/\.(html|js|jsx|ts|tsx|vue)$/, 'gm'),
   width: new RegExp(/\$INTvw/, 'gm'),
   height: new RegExp(/\$INTvh/, 'gm'),
   lineHeight: new RegExp(/\$LINEHEIGHT/, 'gm'),
